61b1c6bd4b5a61dea2c9481404869a2421e701d3
CMHend <cmilesh@live.com>
2022-03-01 08:49:14 -0600

    Set up directories/Schema

---
+++ models/flights.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAC', 'SAN'],
+    default: 'DEN'
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+
+  },
+  departs: {
+    type: Date,
+    default: function() {
+      return new Date().getFullYear()
+    },
+  }
+
+})
